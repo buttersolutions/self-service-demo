3b43a86cf32e02393c83ad841ebdfadb3d110e11
Tomas Krajnik <tkr@allgravy.com>
2026-03-27 15:48:49 +0100

    updating toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${merriweather.variable} antialiased`}>
         {children}
-        <Toaster position="top-center" richColors />
+        <Toaster position="top-center" />
         <Script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
           strategy="afterInteractive"
